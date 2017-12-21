cfab2e82bfc32b13d91731e8179580c248866b05
KendrickA <andrewkendrick37@btinternet.com>
2017-12-21 12:28:17 +0000

    mess

---
+++ test2/src/main/java/test2/Test2.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package test2;
+
+/**
+ * @author KendrickA
+ *
+ */
+public class Test2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("tester");
+
+	}
+
+}
